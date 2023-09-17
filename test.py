1aa55ad3bf6e0c1c644b668577cac5862de89fdd
figarofuga <figarofuga@gmail.com>
2023-09-17 21:17:01 +0900

    add py file

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 #%%
 import pandas as pd
-import xgboost as xgb
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# %%
+# load datasets of titanics from seaborn
+titanic = sns.load_dataset('titanic')
+
+# %%
+# summary of the data
+titanic.info()
+# describe of the data
+titanic.describe()
+# count of the missing values of all columns in the data
+titanic.isnull().sum()
 # %%
